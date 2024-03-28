217e09081c8793de9f5d29fa3188b36c0684a9d1
Rebecca Costanzo <118014916+racostanzo@users.noreply.github.com>
2024-03-28 14:06:52 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,39 +1,4 @@
-terraform { 
-    required_providers {
-        aws = {
-            source = 'hashicorp/aws'
-            version = '~> 4.0'
-        }
-    }
-}
-provider "aws" {
-  region = "us-east-1"  # Adjust the region as per your requirement
-}
-
-# Step 1: Deploy EC2 Instance
-resource "aws_instance" "jenkins_instance" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Choose an appropriate AMI
-  instance_type = "t2.micro"  # Adjust instance type as per your requirement
-  subnet_id     = "subnet-xxxxxxxxxxxxxxx"  # Specify your subnet ID
-
-  tags = {
-    Name = "Jenkins Instance"
-  }
-
-  # Step 2: Bootstrap Instance with Jenkins Installation
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo yum update -y
-              sudo yum install java-1.8.0-openjdk-devel -y
-              sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-              sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-              sudo yum install jenkins -y
-              sudo systemctl start jenkins
-              sudo systemctl enable jenkins
-              EOF
-}
-
-# Step 3: Create Security Group for Jenkins
+#Create Security Group for Jenkins
 resource "aws_security_group" "jenkins_sg" {
   name        = "jenkins-sg"
   description = "Allow port 22 and 8080"
@@ -68,15 +33,48 @@ resource "aws_security_group" "jenkins_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+ tags   = {
+    Name = "jenkins server security group"
+  }
 }
 
-# Step 4: Create S3 Bucket for Jenkins Artifacts
-resource "aws_s3_bucket" "jenkins_artifacts_bucket" {
-  bucket = "jenkins-artifacts-bucket"
-  acl    = "private"
+# use data source to get the latest version of the amazon linux 2 ami
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+   
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
 }
 
-# Output EC2 Public IP
-output "jenkins_public_ip" {
-  value = aws_instance.jenkins_instance.public_ip
+# launch the ec2 instance and install website
+resource "aws_instance" "ec2_instance" {
+  ami                    = data.aws_ami.amazon_linux_2.id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+  key_name               = var.key_name
+  user_data              = file("install_jenkins.sh")
+
+  tags = {
+    Name = "terraform_web_server"
+  }
+}
+
+#create a private S3 bucket
+resource "aws_s3_bucket" "buckettf" {
+  bucket = "luits3-t3rraf0rm-buck3t" #this needs to be globally unique
+
+  tags = {
+    Name        = "TerraformS3bucket"
+    Environment = "Dev"
+  }
 }
